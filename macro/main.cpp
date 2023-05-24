031de40102d6b83e4b4871ba69877126a626f1e9
Aleksey <qwe810@yandex.ru>
2023-05-24 21:50:16 +0700

    done

--- macro/main.cpp
+++ macro/main.cpp
@@ -1,12 +1,18 @@
 #include<iostream>
+#define SUB(a,b) ((a) - (b))
 
-
-int main()
+int main(int argc, char** argv)
 {
-	setlocale(LC_ALL, "Russian");
 
-
-
-
-
+		setlocale(LC_ALL, "Russian");
+		int a = 6;
+		int b = 5;
+		int c = 2;
+		std::cout << SUB(a, b) << std::endl;
+		std::cout << SUB(a, b) * c << std::endl;
+		std::cout << SUB(a, b + c) * c << std::endl;
 }
+
+
+
+
